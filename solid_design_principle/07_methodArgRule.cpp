ed5f436cc4b6a2014c365812a32e46edaeff0689
Aziz-Taufique <taufiqueaziz88@gmail.com>
2025-08-06 11:20:47 +0530

    methodArgRule.cpp

---
+++ solid_design_principle/07_methodArgRule.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+class Parent
+{
+public:
+    virtual void printMsg(std::string msg)
+    {
+        std::cout << "Parent: " << msg << std::endl;
+    }
+};
+
+class Child : public Parent
+{
+public:
+    void printMsg(std::string msg) override
+    {
+        std::cout << "Child " << msg << std::endl;
+    }
+};
+
+class Client
+{
+    Parent *p;
+
+public:
+    Client(Parent *p)
+    {
+        this->p = p;
+    }
+
+    void print(std::string msg)
+    {
+        p->printMsg(msg);
+    }
+};
+
+int main()
+{
+    Parent *parent = new Parent();
+    Parent *child = new Child();
+
+    // Client *client = new Client(parent);
+    Client *client = new Client(child);
+    client->print("looo");
+    return 0;
+}
